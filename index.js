b267c9535f146e6c1a24a02e3d9e867d5c1a92a3
Paul Kim <ezikial142857@gmail.com>
2022-03-27 13:13:14 +0900

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,6 @@ window.onload = () => {
   let groupCount = 0
   let columnMaxCount
   let rowMaxCount
-  let isAllDCare;
   const createTable = document.getElementById(`createTable`)
   createTable.addEventListener(`click`, () => {
     const inputTag = document.getElementById(`degree`)
